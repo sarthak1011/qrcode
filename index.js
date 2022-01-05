4a52732535a904085e5bae62c874cddb208cd566
sarthak <sarth1097@gmail.com>
2022-01-06 00:32:08 +0530

    added open

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const port = process.env.PORT || 3000
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+let a =  open('https://livehealth.solutions/qr_for_userwise_reports/18021/7cf03e3f-b6f9-11eb-b420-0af7484678b0/58255766/');
+  res.send(a)
 
 // opens the url in the default browser 
-// open('https://livehealth.solutions/qr_for_userwise_reports/18021/7cf03e3f-b6f9-11eb-b420-0af7484678b0/58255766/');
 
 })
 
