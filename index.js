ccad8965de5c9949a4654cc611174a59abd6027e
sarthak <sarth1097@gmail.com>
2022-01-06 00:29:32 +0530

    changes

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 
 const express = require('express')
+const open = require('open');
 const app = express()
 const port = process.env.PORT || 3000
 
-/* 
-    Incase you are using mongodb atlas database uncomment below line
-    and replace "mongoAtlasUri" with your mongodb atlas uri.
-*/
-// mongoose.connect( mongoAtlasUri, {useNewUrlParser: true, useUnifiedTopology: true})
+
 
 app.get('/', (req, res) => {
-const open = require('open');
+  res.send('Hello World!')
+
 // opens the url in the default browser 
-open('https://livehealth.solutions/qr_for_userwise_reports/18021/7cf03e3f-b6f9-11eb-b420-0af7484678b0/58255766/');
+// open('https://livehealth.solutions/qr_for_userwise_reports/18021/7cf03e3f-b6f9-11eb-b420-0af7484678b0/58255766/');
 
 })
 
